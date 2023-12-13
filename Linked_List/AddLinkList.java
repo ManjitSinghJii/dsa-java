6d5e1a33f10d92f8f782dc14b4f0013ee665772b
Manjit Singh <122924245+ManjitSinghJii@users.noreply.github.com>
2023-12-13 20:58:47 +0000

    add lasst

--- Linked_List/AddLinkList.java
+++ Linked_List/AddLinkList.java
@@ -13,6 +13,9 @@ class Node{
 class LinkList{
     
     Node head;
+
+    // Print Link List................
+    
     void printLinkList(){
         Node curr = this.head;
         while (curr != null) {
@@ -40,6 +43,50 @@ class LinkList{
 
         }
     }
+
+
+void addFirst(int data){
+
+    // Add First Node In LinkList.................
+
+        Node temp = new Node(data);
+        if(head == null){
+            head = temp;
+        }
+        else{
+            temp.next = head;
+            head = temp;
+        }
+    }
+
+
+    void addIndex(int index, int data){
+
+
+        //       How to Add Element in Link List using Index.............
+
+
+        if(index == 0){
+            addFirst(data);
+        }
+        else{
+            Node temp = new Node(data);
+            int count = 0;
+            //Node curr=this.head;
+            Node curr=head;
+            while (count < index-1) {
+                curr = curr.next;
+                count++;
+            }
+            temp.next=curr.next;
+            curr.next=temp;
+        }
+
+    }
+
+
+
+
 }
 
 public class AddLinkList {
@@ -49,6 +96,10 @@ public class AddLinkList {
 
         ll.add(10);
         ll.add(20);
+        ll.add(30);
+        ll.addIndex(1, 100);
+
+        ll.addFirst(5);
         ll.printLinkList();
 
 
